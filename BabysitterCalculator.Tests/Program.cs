da9b9413f43fb43426985b3f2e0c4bcb6fca2504
Joe Dluzen <jdluzen@gmail.com>
2015-08-02 11:50:30 -0400

    Update packages, add test project

---
+++ BabysitterCalculator.Tests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PetaTest;
+
+namespace BabysitterCalculator.Tests
+{
+    [TestFixture]
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            new Runner().Run(args);
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
